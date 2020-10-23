0316d161fa6348e6e12494582e1ef7d77adb4a73
Oleg Solomka <olsolomk@microsoft.com>
2020-10-23 18:22:30 +0000

    create app utl output so the port is autoforwarded

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app is running at http://localhost:${port}`));
