2779429b1c802953f0736073798850e9d15dbaa6
Osvaldo Ortega <osortega@microsoft.com>
2025-05-04 23:58:01 -0700

    Test

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ app.get("/", (req, res) => {
 
 const b = [1, 2, 3, 4, 5];
 
-// This comment shouldn't be included
-app.test("/", (req, res) => {
-  console.log(`${new Date()} ${req.method} ${req.path}`);
-  // Fake comment
-  // That should be included
-  res.send("Post");
-});
-
 app.use("/", patchHandler);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
