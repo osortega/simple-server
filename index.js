55ebe8bfa8c0b96edb81c48df24d0e8719815bee
Osvaldo Ortega <osortega@microsoft.com>
2025-06-26 16:26:22 -0700

    Testing merge 1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const patchHandler = require("./patchHandler");
 const app = express();
 const port = 3001;
+const testing = 'testing';
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
