69b4c9bc4866685e0ce173e2eb922cd977a679b3
Osvaldo Ortega <osortega@microsoft.com>
2026-02-26 10:38:53 -0800

    Update server port to 9849

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const patchHandler = require("./patchHandler");
 const app = express();
-const port = 5001;
+const port = 9849;
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
