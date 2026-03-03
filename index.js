4a9f3b4dd1236eb2972c24bee44f0cc75a537c2d
Osvaldo Ortega <osortega@microsoft.com>
2026-03-03 14:41:58 -0800

    Revert server port to 824

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const patchHandler = require("./patchHandler");
 const app = express();
-const port = 9849;
+const port = 824;
 
 app.get("/", (req, res) => {
   console.log(`${new Date()} ${req.method} ${req.path}`);
